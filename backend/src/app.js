7d2321f740ad8cebc60423e7299e30dd50268262
SG <sayanghosh.webdev@gmail.com>
2026-05-29 10:04:34 +0530

    feat: add Cloudinary integration and implement middleware for JWT verification and file uploads

---
+++ backend/src/app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+// ================= MIDDLEWARE =================
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+
+app.use(express.json({ limit: "4mb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+// ================= ROUTES =================
+
+app.use("/api/v1/status", (req, res) =>
+    res.send("  Server is ready !!!")
+);
+
+
+import userRouter from "./routes/user.routes.js";
+
+
+app.use("/api/v1/auth", userRouter);
+
+
+
+export { app };
