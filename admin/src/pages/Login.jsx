91ad0b679c10a9c382a1ee9883d875f5d1838b98
SG <sayanghosh.webdev@gmail.com>
2026-07-28 19:05:05 +0530

    feat: integrate Tailwind CSS, implement login page, and set up context providers
    - Added Tailwind CSS dependencies and configured Vite to use it.
    - Removed old CSS styles and replaced them with Tailwind utility classes.
    - Created Admin, App, and Doctor context providers for state management.
    - Implemented a new Login page with state management for Admin and Doctor roles.
    - Updated main application structure to include context providers and routing.

---
+++ admin/src/pages/Login.jsx
@@ -0,0 +1,71 @@
+import  { useState } from 'react';
+//import { assets } from '../assets/assets';
+import { useContext } from 'react';
+import { AdminContext } from '../context/AdminContext.jsx';
+
+const Login = () => {
+  const [state, setState] = useState('Admin');
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { setAToken, backendUrl } = useContext(AdminContext);
+
+  return (
+  <form className="min-h-[80vh] flex items-center ">
+    <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg">
+      <p className="text-2xl font-semibold m-auto">
+        <span className="text-primary">{state}</span> Login
+      </p>
+
+      <div className="w-full">
+        <p>Email</p>
+        <input
+          className="border border-[#DADADA] rounded w-full p-2 mt-1"
+          type="email"
+          onClick={(e)=>setEmail(e.target.value)}
+          value={email}
+          required
+        />
+      </div>
+
+      <div className="w-full">
+        <p>Password</p>
+        <input
+          className="border border-[#DADADA] rounded w-full p-2 mt-1"
+          type="password"
+          required
+        />
+      </div>
+
+      <button className="bg-primary bg-blue-400 text-white  w-full py-2  rounded-md text-base">
+        Login
+      </button>
+
+      {state === "Admin" ? (
+        <p>
+          Doctor Login?{" "}
+          <span
+            onClick={() => setState("Doctor")}
+            className="text-primary underline cursor-pointer"
+          >
+            Click here
+          </span>
+        </p>
+      ) : (
+        <p>
+          Admin Login?{" "}
+          <span
+            onClick={() => setState("Admin")}
+            className="text-primary underline cursor-pointer"
+          >
+            Click here
+          </span>
+        </p>
+      )}
+    </div>
+  </form>
+);
+};
+
+export default Login;
